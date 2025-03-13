4c63fde19bf4a5ea23067698a8541c558ffb9d7a
Sebastian Puerta <sepuertabe@gmail.com>
2025-03-13 09:59:01 -0500

    Se crearon clases para el step definition de GET y su respectivo runner

---
+++ src/test/java/stepDefinitions/GetStepDefinition.java
@@ -0,0 +1,12 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.When;
+
+public class GetStepDefinition extends EstablecerServicio{
+
+    @When("consume el recurso de búsqueda {string}")
+    public void consumeElRecursoDeBusqueda(String endpoint) {
+        // Código para GET
+    }
+
+}
