ac0c4a54dab76604fb8e22955486c4d019cf0199
Sebastian Puerta <sepuertabe@gmail.com>
2025-03-13 10:18:15 -0500

    Actualización de clase GetStepDefinition para incluir la llamada a los servicios GET previamente creados

--- src/test/java/stepDefinitions/GetStepDefinition.java
+++ src/test/java/stepDefinitions/GetStepDefinition.java
@@ -1,12 +1,15 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.When;
+import tasks.ConsumirServicioGet;
 
 public class GetStepDefinition extends EstablecerServicio{
 
     @When("consume el recurso de búsqueda {string}")
-    public void consumeElRecursoDeBusqueda(String endpoint) {
-        // Código para GET
+    public void consumeElRecursoDeBusqueda(String recurso) {
+        actor.attemptsTo(
+                ConsumirServicioGet.conRecurso(recurso)
+        );
     }
 
 }
